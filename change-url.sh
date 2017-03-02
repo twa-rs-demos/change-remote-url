c9054c6038e464f80a31b0ee86e39c99c896d171
Afaren <550047450@qq.com>
2017-03-02 16:33:43 +0800

    afaren - extract to method

--- change-url.sh
+++ change-url.sh
@@ -1,39 +1,23 @@
 #! /usr/bin/env bash
 
-git remote -v
-git remote set-url origin git@github.com:tws-system/recruiting-system.git 
-git remote -v
+set -e 
+
+change() {
+	cd $1
+	echo
+	echo before 
+	git remote -v 
+	git remote set-url origin git@github.com:tws-system/recruiting-system$2.git
+	echo after 
+	git remote -v
+	echo 
+	cd -
+}
 
 
-cd web
-git remote -v
-git remote set-url origin git@github.com:tws-system/recruiting-system-web.git 
-git remote -v
-cd -
-
-cd web-api
-git remote -v
-git remote set-url origin git@github.com:tws-system/recruiting-system-express-api.git 
-git remote -v
-cd -
-
-cd paper-api
-git remote -v
-git remote set-url origin git@github.com:tws-system/recruiting-system-paper-api.git 
-git remote -v
-cd -
-
-cd teacher-admin-web
-git remote -v
-git remote set-url origin git@github.com:tws-system/recruiting-system-teacher-web.git 
-git remote -v
-cd -
-
-cd automated-test
-git remote -v
-git remote set-url origin git@github.com:tws-system/recruiting-system-automated-test.git 
-git remote -v
-cd -
-
-
-
+change . ""
+change web "-web"
+change web-api "-express-api"
+change paper-api "-paper-api"
+change teacher-admin-web "-teacher-web"
+change automated-test "-automated-test"
